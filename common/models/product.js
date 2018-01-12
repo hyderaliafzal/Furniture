2a4b368e501937fdbc4a734197e0c99822dd8ebe
Hyder Ali Afzal <hyderaliafzal@github.com>
2018-01-12 23:21:04 +0500

    product category mapping

--- common/models/product.js
+++ common/models/product.js
@@ -1,5 +1,17 @@
 'use strict';
 
 module.exports = function(Product) {
-
+    Product.beforeRemote('create', (ctx, _instance_, next) => {
+        Product.app.models.Category.find({where: {name: ctx.args.data.category}}, (err, category) => {
+            if(category.length > 0) {
+                ctx.args.data.categoryId = category[0].id;
+                next();
+            } else {
+                Product.app.models.Category.create({name: ctx.args.data.category}, (err, res) => {
+                    ctx.args.data.categoryId = res.id;
+                    next();
+                });
+            }
+        });     
+    });
 };
