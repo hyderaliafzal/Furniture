e1479f827dc41dfc7b106c383298f6b7599eeee5
Hyder Ali Afzal <hyderaliafzal@github.com>
2018-01-13 15:21:21 +0500

    Account creation with role

--- common/models/account.js
+++ common/models/account.js
@@ -1,5 +1,27 @@
 'use strict';
 
 module.exports = function(Account) {
-
+    delete Account.validations.email;
+    Account.observe('after save', (ctx, next) => {
+       if(isNewInstance === true){
+            Account.app.models.Role.find({where: {name: ctx.instance.role}}, (err, role) => {
+                if(err) {
+                    next(err, null);
+                }
+                let rolemap = {
+                   principalType: 'USER',
+                   principalId: ctx.instance.id,
+                   roleId: role[0].id 
+                }
+                Account.app.models.RoleMapping.create(rolemap, (err, res) => {
+                    if(err){
+                        next(err, null);
+                    }
+                    next();
+                });
+            });
+       }else{
+           next();
+       }
+    });
 };
