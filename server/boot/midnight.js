2353aec65564d013160c164cae644a985afb3c6f
Hyder Ali Afzal <ali.afzal@blueeast.com>
2018-02-20 14:10:19 +0500

    cron stop

--- server/boot/midnight.js
+++ server/boot/midnight.js
@@ -14,31 +14,31 @@ weekday[6] = 'Saturday';
 var n = weekday[d.getDay()];
 var j = schedule.scheduleJob('* * * * * *', function() {
   let brandpayments = [];
-  models.Shop.find({}, (err, shops) => {
-    if (shops) {
-      shops.map(shop => {
-        for (let count = 0; count < 7; count++) {
-          let date = moment().subtract(count, 'day').format('YYYY-M-DD').split('-');
-          console.log(shop.id, date);
-          models.Bill.find({where: {shopId: shop.id, day: date[2], month: date[1], year: date[0]}},
-            (err, bills) => {
-              if(bills.length>0){
-                bills.map(bill => {
-                  if(brandpayments.length === 0){
-                    let basePrice = 0;
-                    bill._products.map(p=>{
-                      basePrice = parseInt(p.basePrice) * parseInt(p.quantity);
-                    });
-                    brandpayments = [{shopId: shop.id, day: date[2], month: date[1], year: date[0],
-                                      basePrice: basePrice}]  
-                  }
-                });  
-              }
-          });
-        }
-      });
-    }
-  });
+  // models.Shop.find({}, (err, shops) => {
+  //   if (shops) {
+  //     shops.map(shop => {
+  //       for (let count = 0; count < 7; count++) {
+  //         let date = moment().subtract(count, 'day').format('YYYY-M-DD').split('-');
+  //         console.log(shop.id, date);
+  //         models.Bill.find({where: {shopId: shop.id, day: date[2], month: date[1], year: date[0]}},
+  //           (err, bills) => {
+  //             if(bills.length>0){
+  //               bills.map(bill => {
+  //                 if(brandpayments.length === 0){
+  //                   let basePrice = 0;
+  //                   bill._products.map(p=>{
+  //                     basePrice = parseInt(p.basePrice) * parseInt(p.quantity);
+  //                   });
+  //                   brandpayments = [{shopId: shop.id, day: date[2], month: date[1], year: date[0],
+  //                                     basePrice: basePrice}]  
+  //                 }
+  //               });  
+  //             }
+  //         });
+  //       }
+  //     });
+  //   }
+  // });
   if (n === 'Monday') {
 
   }
