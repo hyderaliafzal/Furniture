4b564d5c58ccc94eb9a8a1db2388a886e91fd0f5
Hyder Ali Afzal <ali.afzal@blueeast.com>
2018-02-23 15:22:07 +0500

    midnight

--- server/boot/midnight.js
+++ server/boot/midnight.js
@@ -13,62 +13,74 @@ weekday[5] = 'Friday';
 weekday[6] = 'Saturday';
 var n = weekday[d.getDay()];
 var j = schedule.scheduleJob('* * * * * *', function() {
-  let brandpayments = [];
-/*  findShop().then(shops => {
-    if (shops.length > 0) {
-      shops.map(shop => {
-        for (let count = 0; count < 7; count++) {
-          let date = moment().subtract(count, 'day').format('YYYY-M-DD').split('-');
-          shopWiseBills(shop.id, date[2], date[1], date[0])
-            .then(bills => {
-              // brandpayments;
-              return new Promise(bills.map(bill => {
-                // brandpayments.push({shopId: bill.shopId, bill.brand})
-              })).then(() => {
-
-              })
-            });
-        }
-      });
-    }
-  });*/
-  /* models.Shop.find({}, (err, shops) => {
-    if (shops) {
-      shops.map(shop => {
-        for (let count = 0; count < 7; count++) {
-          let date = moment().subtract(count, 'day').format('YYYY-M-DD').split('-');
-          console.log(shop.id, date);
-          models.Bill.find({where: {shopId: shop.id, day: date[2], month: date[1], year: date[0]}},
-            (err, bills) => {
-              if (bills.length > 0) {
-                bills.map(bill => {
-                  if (brandpayments.length === 0) {
-                    let basePrice = 0;
-                    bill._products.map(p=> {
-                      basePrice = parseInt(p.basePrice) * parseInt(p.quantity);
-                      return brandpayments.push({
-                        brand: p.brandName,
-                        shopId: shop.id,
-                        day: date[2],
-                        month: date[1],
-                        year: date[0],
-                        basePrice: basePrice,
-                      });
-                      console.log(brandpayments);
-                    });
-                  }
-                });
-              }
-            });
-        }
-      });
-    }
-  }); */
   if (n === 'Monday') {
 
   }
 });
 
+let brandpayments = [];
+findShop().then(shops => {
+  if (shops.length > 0) {
+    shops.map(shop => {
+      for (let count = 0; count < 7; count++) {
+        let date = moment().subtract(count, 'day').format('YYYY-M-DD').split('-');
+        shopWiseBills(shop.id, date[2], date[1], date[0]).then(bills => {
+          let billsCount = 0;
+          if (bills.length > 0) {
+            return new Promise(resolve => {
+              let billsCount = 0;
+              bills.map(bill => {
+                let productCount = 0;
+                return new Promise(resolve2 => {
+                  bill._products.map(product => {
+                    brandpayments.push({
+                      shopId: shop.id,
+                      brand: product.brand,
+                      amount: parseInt(product.quantity) * parseInt(product.basePrice)});
+                    productCount++;
+                    if (productCount === bill._products.length) {
+                      return resolve2();
+                    }
+                  });
+                }).then(() => {
+                  billsCount++;
+                  if (billsCount === bills.length) {
+                    return resolve();
+                  }
+                });
+              });
+            }).then(() => {
+              let finalBPayment = [];
+              return new Promise(resolve => {
+                let count = 0;
+                brandpayments.map(bpayment => {
+                  if (finalBPayment.length === 0) {
+                    finalBPayment.push(bpayment);
+                  } else {
+                    finalBPayment.map((fbp, index) => {
+                      if (fbp.brand === bpayment.brand) {
+                        finalBPayment[index].amount += parseInt(bpayment.amount);
+                      } else {
+                        finalBPayment.push(bpayment);
+                      }
+                    });
+                  }
+                  count++;
+                  if (count === brandpayments.length) {
+                    resolve();
+                  }
+                });
+              }).then(() => {
+                console.log(finalBPayment, d);
+                // models.BrandDues.create()
+              });
+            });
+          }
+        });
+      }
+    });
+  }
+});
 function findShop() {
   return new Promise((resolve, reject) => {
     models.Shop.find((err, shops) => {
@@ -79,39 +91,10 @@ function findShop() {
 
 function shopWiseBills(shopId, day, month, year) {
   return new Promise((resolve, reject) => {
-    models.Bills.find({shopId: shopId, day: day, month: month, year: year},
+    models.Bill.find({where:
+          {shopId: shopId, day: day, month: month, year: year}},
       (err, bills) => {
         return resolve(bills);
       });
-  });
-}
-
-function amountCalculation(shopId, bills) {
-  return Promise.all([billScan(bills)]).then(result => {
-    return result;
-  });
-}
-
-function billScan(bills) {
-  return new Promise(resolve => {
-    bills.map(bill => {
-      resolve(productBrandMap(bill._products));
-    });
-  });
-}
-
-function productBrandMap(shopId, products) {
-  return new Promise(resolve => {
-    products.map(product => {
-      resolve(brandProductCalculation(shopId, product));
-    });
-  });
-}
-
-function brandProductCalculation(shopId, product) {
-  return new Promise(resolve => {
-    console.log(product);
-    resolve({shopId, brand: product.brandName,
-      basePrice: (product.quantity * product.basePrice)});
   });
 }
