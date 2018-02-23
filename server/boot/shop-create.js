dd1d8f37aadcd904f0323897e00d388489e15b7b
Hyder Ali Afzal <ali.afzal@blueeast.com>
2018-02-23 17:17:17 +0500

    shops names channging

--- server/boot/shop-create.js
+++ server/boot/shop-create.js
@@ -3,10 +3,10 @@ module.exports = function(app) {
   app.models.Shop.find((err, res) => {
     console.log(res);
     if (res.length < 2) {
-      const shops = [{name: 'Citizen',
-        address: 'Shahdra, Lahore'},
-      {name: 'Woodworks',
-        address: 'Gulberg III, Lahore'}];
+      const shops = [{name: '"Citizen Plastic & Steel Furnitire',
+        address: 'Main G.T Road, Ravi Toll Plaza, Lahore'},
+      {name: '"Citizen Plastic & Steel Furnitire',
+        address: 'Main College Road Near Al-Janat Marriage Hall, Khokhar Chowk, Township, Lahore}];
 
       shops.forEach(shop => {
         app.models.Shop.create(shop, (err, res) => {
